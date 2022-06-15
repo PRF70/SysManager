d4ed733043497486819f5ac41a8682c4e6ba6b20
Paulo Roberto Fernandes <fernandes.pauloroberto@gmail.com>
2022-06-15 20:29:22 -0300

    Inicio de Projeto!

---
+++ SysManager.API.Admin/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SysManager.API.Admin
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
